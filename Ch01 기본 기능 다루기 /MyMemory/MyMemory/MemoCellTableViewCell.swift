c4eeba72d98dd42028dc0e99c9120ee32a420116
chris69223 <chris6923@daum.net>
2021-04-26 16:09:07 +0900

    gitignore 설정

---
+++ Ch01 기본 기능 다루기 /MyMemory/MyMemory/MemoCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCellTableViewCell.swift
+//  MyMemory
+//
+//  Created by 유저 on 2021/04/26.
+//
+
+import UIKit
+
+class MemoCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Ch01 기본 기능 다루기 /MyMemory/MyMemory/MemoData.swift
@@ -0,0 +1,8 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by 유저 on 2021/04/26.
+//
+
+import Foundation
