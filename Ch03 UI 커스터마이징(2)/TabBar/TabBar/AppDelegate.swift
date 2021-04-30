7e39bd067df87aec416a0c86c2bfc12cc415f7ee
chris69223 <chris6923@daum.net>
2021-04-30 15:41:26 +0900

    TapBar

--- Ch03 UI 커스터마이징(2)/TabBar/TabBar/AppDelegate.swift
+++ Ch03 UI 커스터마이징(2)/TabBar/TabBar/AppDelegate.swift
@@ -9,14 +9,28 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 1) 루트 뷰 컨트롤러를 UITabBarController로 캐스팅한다.
+        if let tbC = self.window?.rootViewController as? UITabBarController {
+          // 2) 탭 바로부터 탭 바 아이템 배열을 가져온다.
+          if let tbItems = tbC.tabBar.items {
+            // 3) 탭 바 아이템에 커스텀 이미지를 등록한다.
+            tbItems[0].image = UIImage(named: "calendar")
+            tbItems[1].image = UIImage(named: "file-tree")
+            tbItems[2].image = UIImage(named: "photo")
+        
+        // 4) 탭 바 아이템에 타이틀을 설정한다.
+        tbItems[0].title = "calendar"
+        tbItems[1].title = "file"
+        tbItems[2].title = "photo"
+          }
+        }
         return true
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -31,6 +45,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
