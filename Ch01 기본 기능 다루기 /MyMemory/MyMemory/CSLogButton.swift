c9482fe8c293a8b5c03b08bdd3cc538f8b86bd5b
chris69223 <chris6923@daum.net>
2021-05-22 22:26:15 +0900

    add CSlogButton

--- Ch01 기본 기능 다루기 /MyMemory/MyMemory/CSLogButton.swift
+++ Ch01 기본 기능 다루기 /MyMemory/MyMemory/CSLogButton.swift
@@ -15,5 +15,25 @@ public enum CSLogType:Int{
 class CSLogButton: UIButton {
     public var logType: CSLogType = .basic
     
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //버튼에 스타일 적용
+        self.setBackgroundImage(UIImage(named: "button-bg"), for: .normal)
+        self.tintColor = .white
+        
+        self.addTarget(self, action: #selector(logging(_:)), for: .touchUpInside)
+    }
+    
+    @objc func logging(_ sender: UIButton){
+        switch self.logType {
+        case .basic: // 단순 로그만 출력
+            NSLog("버튼이 클릭되었습니다.")
+        case .title:
+            let btnTitle = sender.titleLabel?.text ?? "타이틀 없는"
+            NSLog("\(btnTitle) 버튼이 클릭되었습니다.")
+        case .tag:
+            NSLog("\(sender.tag) 버튼이 클릭되었습니다.")
+        }
+    }
 
 }
